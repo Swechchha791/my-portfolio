a1d2f7efd0eac62e6851a1556ce4528626edf080
Swechchha <swechchhapandey271@gmail.com>
2025-03-15 09:04:50 +0530

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import resume from "../assets/my-resume.pdf";
